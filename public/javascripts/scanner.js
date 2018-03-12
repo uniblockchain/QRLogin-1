0cc85eaf31f84fc667fd92badf7cee2a531b065e
Richard Ng <richard.ng@corp.agscreative.com>
2018-03-12 15:58:05 +0800

    Refactor startup

--- public/javascripts/scanner.js
+++ public/javascripts/scanner.js
@@ -3,77 +3,64 @@
     if (window.QRLogin) {
       clearInterval(int)
       var QRLogin = window.QRLogin
-
-
-      if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
-        return ('mediaDevices / getUserMedia not available in your browser')
-      }
-
       var socket = window.QRLogin.socket
 
-      var width = window.innerWidth * 2/3;    // We will scale the photo width to this
-      var height = 0;     // This will be computed based on the input stream
-
-      var streaming = false;
-
-      var video = null;
-      var photo = null;
-      var startbutton = null;
-
-      var deviceId = null;
-
-      function startup() {
-        video = document.getElementById('video');
-        photo = document.getElementById('photo');
-        startbutton = document.getElementById('startbutton');
-
-        navigator.mediaDevices.getUserMedia({ video: { facingMode: { exact: "environment" } }, audio: false })
-          .then(function (stream) {
-            video.srcObject = stream;
-
-            function sleep(ms = 0) {
-              return new Promise(r => setTimeout(r, ms));
-            }
-
-            sleep(1000).then(function () {
-              const rangeInput = document.querySelector('input[type="range"]');
-              const track = stream.getVideoTracks()[0];
-              const capabilities = track.getCapabilities();
-              if ('zoom' in capabilities) {
-                track.applyConstraints({advanced: [ {zoom: capabilities.zoom.max *0.75 } ]});
-              }
-              if ('focusMode' in capabilities) {
-                track.applyConstraints({advanced: [ {focusMode: 'continuous'} ]});
-              }
-            });
-          })
-          .catch(function(err) {
-              alert(err);
-          });
-
-        // sizing the video once according to stream
-        video.addEventListener('canplay', function(ev){
-          if (!streaming) {
-            height = video.videoHeight / (video.videoWidth/width);
-
-            video.setAttribute('width', width);
-            video.setAttribute('height', height);
-            streaming = true;
-
-            searchQRcode(video, function (res) {
-              $("#status").text("Scan Success")
-              socket.emit('device scanning done', res, parseQueryString(location).loginId)
-            });
-          }
-        }, false);
-      } // startup end
-
-      startup()
-
+      var video = document.getElementById('video');
+      startup(video, function (video) {
+        searchQRcode(video, function (res) {
+          $("#status").text("Scan Success")
+          socket.emit('device scanning done', res, parseQueryString(location).loginId)
+        });
+      })
     }
   }); // interval end
 
   // utils
+  function startup(video, onVideoReady) {
+    if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+      return ('mediaDevices / getUserMedia not available in your browser')
+    }
+
+    var width = window.innerWidth * 2/3;    // We will scale the photo width to this
+    var height = 0;     // This will be computed based on the input stream
+    var streaming = false;
+
+    navigator.mediaDevices.getUserMedia({ video: { facingMode: { exact: "environment" } }, audio: false })
+      .then(function (stream) {
+        video.srcObject = stream;
+        video.play()
+
+        // zooming
+        setTimeout(function () {
+          const rangeInput = document.querySelector('input[type="range"]');
+          const track = stream.getVideoTracks()[0];
+          const capabilities = track.getCapabilities();
+          if ('zoom' in capabilities) {
+            track.applyConstraints({advanced: [ {zoom: capabilities.zoom.max *0.75 } ]});
+          }
+          if ('focusMode' in capabilities) {
+            track.applyConstraints({advanced: [ {focusMode: 'continuous'} ]});
+          }
+        }, 1000)
+      })
+      .catch(function(err) {
+          alert(err);
+      });
+
+    // dimensioning the video and run callback
+    video.addEventListener('canplay', function(ev){
+      if (!streaming) {
+        height = video.videoHeight / (video.videoWidth/width);
+
+        video.setAttribute('width', width);
+        video.setAttribute('height', height);
+        streaming = true;
+
+        onVideoReady(video)
+      }
+    }, false);
+  } // startup end
+
   function searchQRcode(video, successCb) {
     var canvas = document.createElement("canvas")
     var context = canvas.getContext('2d');
@@ -97,8 +84,7 @@
     }
 
     qrcode.decode(data);
-
-  } // searchQRcode end
+  }
 
   function parseQueryString( location ) {
     var queryString = location.search.substring(1);
